186427e04a81b2adc58b92d41f5e0fc8c23799c8
André <andremgmonteiro01@gmail.com>
2024-11-09 15:04:58 +0000

    Handle Client Connection Close

--- Client/Views/MainView.axaml.cs
+++ Client/Views/MainView.axaml.cs
@@ -4,64 +4,71 @@ using System.IO;
 using System.Net.Sockets;
 using System;
 using System.Threading;
+using Avalonia;
 using ESR.Shared;
 
 namespace Client.Views;
 
-public partial class MainView : UserControl {
-	private const int c_Port = 5000;
-	private UdpClient _udpClient;
-	private bool _isRunning = true;
+public partial class MainView : UserControl
+{
+    private const int c_Port = 5000;
+    private UdpClient _udpClient;
+    private TcpClient _tcpClient;
+    private bool _isRunning = true;
+    
+    public MainView()
+    {
+        InitializeComponent();
+        AskServerForVideo();
+        StartUdpStream();
+    }
+    
+    private async void StartUdpStream()
+    {
+        _udpClient = new UdpClient(Consts.UdpPort + 1);
 
-	public MainView() {
-		InitializeComponent();
-		AskServerForVideo();
-		StartUdpStream();
-	}
+        try
+        {
+            while (_isRunning)
+            {
+                var result = await _udpClient.ReceiveAsync();
+                var frameBuffer = result.Buffer;
 
-	~MainView()
-	{
-		StopUdpStream();
-	}
+                DisplayFrame(frameBuffer);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error receiving UDP stream: {ex.Message}");
+        }
+        finally
+        {
+            Close();
+        }
+    }
 
-	private async void StartUdpStream() {
-		_udpClient = new UdpClient(Consts.UdpPort + 1);
+    private void DisplayFrame(byte[] frameBuffer)
+    {
+        using var stream = new MemoryStream(frameBuffer);
 
-		try {
-			while (_isRunning) {
-				Console.WriteLine("Receiving UDP stream...");
-				var result = await _udpClient.ReceiveAsync();
-				var frameBuffer = result.Buffer;
+        var bitmap = new Bitmap(stream);
+        StreamImage.Source = bitmap;
+    }
 
-				DisplayFrame(frameBuffer);
-			}
-		} catch (Exception ex) {
-			Console.WriteLine($"Error receiving UDP stream: {ex.Message}");
-		} finally {
-			_udpClient.Close();
-		}
-	}
+    public void Close()
+    {
+        _isRunning = false;
+        _udpClient.Close();
+        _tcpClient.Close();
+    }
 
-	private void DisplayFrame(byte[] frameBuffer) {
-		using var stream = new MemoryStream(frameBuffer);
+    private void AskServerForVideo()
+    {
+        Console.WriteLine("Asking server for video...");
+        _tcpClient = new TcpClient();
+        _tcpClient.Connect("127.0.0.1", Consts.TcpPort);
 
-		var bitmap = new Bitmap(stream);
-		StreamImage.Source = bitmap;
-	}
-
-	public void StopUdpStream() {
-		_isRunning = false;
-		_udpClient.Close();
-	}
-
-	private void AskServerForVideo() {
-		Console.WriteLine($"Asking server for video...");
-		var client = new TcpClient();
-		client.Connect("127.0.0.1", Consts.TcpPort);
-		
-		var stream = client.GetStream();
-		stream.WriteByte(0x01);
-
-		client.Close();
-	}
+        var stream = _tcpClient.GetStream();
+        stream.WriteByte(0x01);
+    }
 }
--- Client/Views/MainWindow.axaml.cs
+++ Client/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using Avalonia.Controls;
 
 namespace Client.Views;
 
@@ -7,5 +8,12 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        
+        Closing += (_, _) => OnClosing();
+    }
+
+    private void OnClosing()
+    {
+        if (Content is MainView mainView) mainView.Close();
     }
 }
