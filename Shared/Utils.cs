eeff231b08a7c3c7fd6b59121ea8355c9ba961a1
André <andremgmonteiro01@gmail.com>
2024-11-09 17:27:43 +0000

    Tracker & Node Barebones

---
+++ Shared/Utils.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace ESR.Shared;
+
+public static class Utils
+{
+    public static string? GetIPAddressFromTcpClient(TcpClient tcpClient)
+    {
+        return (tcpClient.Client.RemoteEndPoint as IPEndPoint)?.Address.ToString().Split(":")[0];
+    } 
+}
