946069a41c613fc1178be3a65872075e7d37b9d7
André <andremgmonteiro01@gmail.com>
2024-11-14 13:41:12 +0000

    Linux IPs

--- Node/Program.cs
+++ Node/Program.cs
@@ -5,62 +5,75 @@ using ESR.Shared;
 
 namespace ESR.Node
 {
-	internal static class Program
-	{
-		public static List<string> Connections = [];
-		
-		private static void Main()
-		{
-			Console.WriteLine("[Tracker] Asking Tracker For Nearby Nodes...");
+    internal static class Program
+    {
+        public static List<string> Connections = [];
 
-			Bootstrap();
+        private static void Main()
+        {
+            Console.WriteLine("[Tracker] Asking Tracker For Nearby Nodes...");
 
-			if (Connections.Count == 0)
-			{
-				throw new Exception("[Tracker] No nodes found.");
-			}
-			else
-			{
-				Console.WriteLine("[Tracker] Found nodes:");
-				foreach (var connection in Connections)
-				{
-					Console.WriteLine(connection);
-				}
-			}
-			
-			var heartBeatSender = new Thread(HeartBeatSender);
-			heartBeatSender.Start();
-			var heartBeatListener = new Thread(HeartBeatListener);
-			heartBeatListener.Start();
-		}
+            Bootstrap();
 
-		private static void Bootstrap()
-		{
-			var response = NetworkMessenger.Get(Consts.TrackerIpAddress, Consts.TcpPort, OpCodes.GetNodes, false);
-			var args = response.Arguments;
-			List<NodeResponse> nodeResponse = [];
-			foreach(var arg in args)
-			{
-				nodeResponse.Add(JsonSerializer.Deserialize<NodeResponse>(arg));
-			}
-			foreach(var node in nodeResponse)
-			{
-				Connections.AddRange(node.Connections);
-			}
-		}
+            if (Connections.Count == 0)
+            {
+                throw new Exception("[Tracker] No nodes found.");
+            }
+            else
+            {
+                Console.WriteLine("[Tracker] Found nodes:");
+                foreach (var connection in Connections)
+                {
+                    Console.WriteLine(connection);
+                }
 
-		private static void HeartBeatSender()
-		{
-			var udpClient = new UdpClient(Consts.UdpPortHeartbeat);
-			
-			
-		}
-		
-		private static void HeartBeatListener()
-		{
-			var udpClient = new UdpClient(Consts.UdpPortHeartbeatResponse);
-			
-			
-		}
-	}
+                NetworkMessenger.DisposeTcpClient(Consts.TrackerIpAddress);
+            }
+
+            var heartBeatSender = new Thread(HeartBeatSender);
+            heartBeatSender.Start();
+            var heartBeatListener = new Thread(HeartBeatListener);
+            heartBeatListener.Start();
+        }
+
+        private static void Bootstrap()
+        {
+            var response = NetworkMessenger.Get(Consts.TrackerIpAddress, Consts.TcpPort, OpCodes.GetNodes, false);
+            var args = response.Arguments;
+            List<NodeResponse> nodeResponse = [];
+            foreach (var arg in args)
+            {
+                nodeResponse.Add(JsonSerializer.Deserialize<NodeResponse>(arg));
+            }
+
+            foreach (var node in nodeResponse)
+            {
+                Connections.AddRange(node.Connections);
+            }
+        }
+
+        private static void HeartBeatSender()
+        {
+            NetworkMessenger.StartUdpClient(Consts.UdpPortHeartbeat, async client =>
+            {
+                foreach (var con in Connections)
+                {
+                    var packet = new PacketBuilder().WriteOpCode(OpCodes.Heartbeat).Packet;
+                    await client.SendAsync(packet, packet.Length, con, Consts.UdpPortHeartbeatResponse);
+                }
+
+                Thread.Sleep(5000);
+            });
+        }
+
+        private static void HeartBeatListener()
+        {
+            NetworkMessenger.StartUdpClient(Consts.UdpPortHeartbeatResponse, async client =>
+            {
+                var result = await client.ReceiveAsync();
+                var ip = result.RemoteEndPoint.Address.ToString();
+                Console.WriteLine($"[Heartbeat] Received heartbeat from {ip}");
+            });
+        }
+    }
 }
